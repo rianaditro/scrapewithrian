ab3b3f1c9acfe7264feb3ddd5586200aad5e63c6
rianaditro <rianaditro@gmail.com>
2025-03-26 09:08:39 +0700

    simplify registered routers by package, add lifespan to count total data on app lifecycle

--- api/backend/__init__.py
+++ api/backend/__init__.py
@@ -1,9 +1,33 @@
-from fastapi import FastAPI
-from api.backend.routers import home
-from api.backend.routers import simple_table
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from fastapi import FastAPI
+from sqlmodel import select
+from sqlalchemy import func
+
+from api.db.session import get_session
+from api.models.products import Product
+from api.backend import routers
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Convert session generator to session
+    session = next(get_session())
+
+    # Count total data
+    statement = select(func.count()).select_from(Product)
+    product_count = session.exec(statement).one()
+
+    # Store total data in app state
+    app.state.product_count = product_count
+
+    session.close()
+
+    yield # yield control back to FastAPI
+
+
+app = FastAPI(lifespan=lifespan)
+router = routers.router
 
 # Register routers
-app.include_router(home.router)
-app.include_router(simple_table.router)
+app.include_router(router)
